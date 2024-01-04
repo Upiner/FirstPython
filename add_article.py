601bd500eb369af4e95722515ec38de63694f11d
Upiner <qustenfaserewe@gmail.com>
2024-01-04 23:52:24 +0300

    Revert "файлы из урока по питону"
    This reverts commit db10df54932361f0356770d9cbca1814f76a63c5.

--- add_article.py
+++
@@ -1,3 +0,0 @@
-from myapp.models.py import Article
-article = Article (title='Заголовок статьи', author='Имя Автора', content = 'Текст статьи')
-article.save()
--- rep/lesson1.py
+++
@@ -1,2 +0,0 @@
-c = open(r'C:\Projects\FirstPython\Firsty\venv\Scripts\requirements.txt', encoding='utf-16')
-print(c.read())
